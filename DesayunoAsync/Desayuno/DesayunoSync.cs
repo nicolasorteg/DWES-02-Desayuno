1f1e7b98969bcc64afae5ec22a1164576ab50d80
nicolasorteg <nicolasortegaf22@gmail.com>
2026-09-20 21:52:46 +0200

    version sincrona

---
+++ DesayunoAsync/Desayuno/DesayunoSync.cs
@@ -0,0 +1,57 @@
+﻿namespace Desayuno;
+
+/// <summary>
+/// Clase con métodos estáticos para simular
+/// la preparación síncrona del desayuno.
+/// </summary>
+public static class DesayunoSync {
+    
+    public static void HacerCafe() {
+        
+        Console.WriteLine("🟢 Empezando: Hacer café");
+        Thread.Sleep(200);
+        Console.WriteLine("🔴 Terminado: Hacer café");
+    }
+
+    public static void CalentarSarten() {
+        
+        Console.WriteLine("🟢 Empezando: Calentar sartén");
+        Thread.Sleep(200);
+        Console.WriteLine("🔴 Terminado: Calentar sartén");
+    }
+
+    public static void FreirHuevos() {
+        
+        Console.WriteLine("🟢 Empezando: Freír huevos");
+        Thread.Sleep(300);
+        Console.WriteLine("🔴 Terminado: Freír huevos");
+    }
+
+    public static void FreirBacon() {
+        
+        Console.WriteLine("🟢 Empezando: Freír bacon");
+        Thread.Sleep(300);
+        Console.WriteLine("🔴 Terminado: Freír bacon");
+    }
+
+    public static void TostarPan() {
+        
+        Console.WriteLine("🟢 Empezando: Tostar pan");
+        Thread.Sleep(200);
+        Console.WriteLine("🔴 Terminado: Tostar pan");
+    }
+
+    public static void UntarMantequilla() {
+        
+        Console.WriteLine("🟢 Empezando: Untar mantequilla");
+        Thread.Sleep(100);
+        Console.WriteLine("🔴 Terminado: Untar mantequilla");
+    }
+
+    public static void HacerZumo() {
+        
+        Console.WriteLine("🟢 Empezando: Hacer zumo");
+        Thread.Sleep(200);
+        Console.WriteLine("🔴 Terminado: Hacer zumo");
+    }
+}
--- DesayunoAsync/Desayuno/Program.cs
+++ DesayunoAsync/Desayuno/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using System.Text;
+using Desayuno;
+
+Console.OutputEncoding = Encoding.UTF8;
+var cont = Stopwatch.StartNew();
+
+Console.WriteLine("-- PREPARACIÓN SÍNCRONA --");
+cont.Restart();
+DesayunoSync.HacerCafe();
+DesayunoSync.CalentarSarten();
+DesayunoSync.FreirHuevos();
+DesayunoSync.FreirBacon();
+DesayunoSync.TostarPan();
+DesayunoSync.UntarMantequilla();
+DesayunoSync.HacerZumo();
+cont.Stop();
+
+Console.WriteLine($"\nDesayuno listo (secuencial) en {cont.ElapsedMilliseconds} ms");
