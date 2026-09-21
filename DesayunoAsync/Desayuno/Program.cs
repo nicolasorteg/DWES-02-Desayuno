15e0dea75f103ff02bd963c18e8db5c97057e53c
nicolasorteg <nicolasortegaf22@gmail.com>
2026-09-21 04:13:31 +0200

    add parallel

--- DesayunoAsync/Desayuno/Program.cs
+++ DesayunoAsync/Desayuno/Program.cs
@@ -32,3 +32,32 @@ await DesayunoAsync.HacerZumo();
 cont.Stop();
 
 Console.WriteLine($"\nDesayuno listo (async secuencial) en {cont.ElapsedMilliseconds} ms");
+
+
+
+Console.WriteLine("\n-- PREPARACIÓN PARALELISMO --");
+cont.Restart();
+var tCafe = DesayunoAsync.HacerCafe();
+var tTostada = PrepararTostadaAsync();
+var tFritos = PrepararFritosAsync();
+var tZumo = DesayunoAsync.HacerZumo();
+await Task.WhenAll(tCafe, tTostada, tFritos, tZumo);
+cont.Stop();
+
+Console.WriteLine($"\nDesayuno listo (paralelo) en {cont.ElapsedMilliseconds} ms");
+return;
+
+// funciones para agrupar acciones que dependen de otras
+async Task PrepararTostadaAsync() {
+    
+    await DesayunoAsync.TostarPan();
+    await DesayunoAsync.UntarMantequilla(); // necesita el pan tostado
+}
+
+async Task PrepararFritosAsync() { 
+    
+    await DesayunoAsync.CalentarSarten();          
+    var tHuevos = DesayunoAsync.FreirHuevos();      
+    var tBacon = DesayunoAsync.FreirBacon();
+    await Task.WhenAll(tHuevos, tBacon);            // necesitan que la sartén esté caliente
+}
