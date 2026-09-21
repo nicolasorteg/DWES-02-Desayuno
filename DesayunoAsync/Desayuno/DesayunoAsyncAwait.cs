5a6686624a910de82e0f5201a2b2e76aee04ce86
nicolasorteg <nicolasortegaf22@gmail.com>
2026-09-21 04:03:52 +0200

    add async/await

---
+++ DesayunoAsync/Desayuno/DesayunoAsyncAwait.cs
@@ -0,0 +1,57 @@
+﻿namespace Desayuno;
+
+/// <summary>
+/// Clase con métodos estáticos para simular
+/// la preparación del desayuno usando async/await.
+/// </summary>
+public static class DesayunoAsync {
+
+    public static async Task HacerCafe() {
+        
+        Console.WriteLine("🟢 Empezando: Hacer café");
+        await Task.Delay(200);
+        Console.WriteLine("🔴 Terminado: Hacer café");
+    }
+
+    public static async Task CalentarSarten() {
+        
+        Console.WriteLine("🟢 Empezando: Calentar sartén");
+        await Task.Delay(200);
+        Console.WriteLine("🔴 Terminado: Calentar sartén");
+    }
+
+    public static async Task FreirHuevos() {
+        
+        Console.WriteLine("🟢 Empezando: Freír huevos");
+        await Task.Delay(300);
+        Console.WriteLine("🔴 Terminado: Freír huevos");
+    }
+
+    public static async Task FreirBacon() {
+        
+        Console.WriteLine("🟢 Empezando: Freír bacon");
+        await Task.Delay(300);
+        Console.WriteLine("🔴 Terminado: Freír bacon");
+    }
+
+    public static async Task TostarPan() {
+        
+        Console.WriteLine("🟢 Empezando: Tostar pan");
+        await Task.Delay(200);
+        Console.WriteLine("🔴 Terminado: Tostar pan");
+    }
+
+    public static async Task UntarMantequilla() {
+        
+        Console.WriteLine("🟢 Empezando: Untar mantequilla");
+        await Task.Delay(100);
+        Console.WriteLine("🔴 Terminado: Untar mantequilla");
+    }
+
+    public static async Task HacerZumo() {
+        
+        Console.WriteLine("🟢 Empezando: Hacer zumo");
+        await Task.Delay(200);
+        Console.WriteLine("🔴 Terminado: Hacer zumo");
+    }
+}
--- DesayunoAsync/Desayuno/Program.cs
+++ DesayunoAsync/Desayuno/Program.cs
@@ -17,3 +17,18 @@ DesayunoSync.HacerZumo();
 cont.Stop();
 
 Console.WriteLine($"\nDesayuno listo (secuencial) en {cont.ElapsedMilliseconds} ms");
+
+
+
+Console.WriteLine("\n-- PREPARACIÓN ASYNC/AWAIT --");
+cont.Restart();
+await DesayunoAsync.HacerCafe();
+await DesayunoAsync.CalentarSarten();
+await DesayunoAsync.FreirHuevos();
+await DesayunoAsync.FreirBacon();
+await DesayunoAsync.TostarPan();
+await DesayunoAsync.UntarMantequilla();
+await DesayunoAsync.HacerZumo();
+cont.Stop();
+
+Console.WriteLine($"\nDesayuno listo (async secuencial) en {cont.ElapsedMilliseconds} ms");
